6d7fd05fd3cac8987ef0d4d7dcd9bf521fda00f7
matsu7874 <mtsmtkmt@gmail.com>
2015-07-08 11:56:31 +0900

    create case_creater.py

---
+++ case_creater.py
@@ -0,0 +1,4 @@
+for i in range(3, 10):
+    print('simulation.exe',i,3,'4.0 1.0 0.0 > log/'+str(i)+'-3-4_0-1_0-0_0.txt')
+    print('simulation.exe',i,3,'2.0 1.0 0.0 > log/'+str(i)+'-3-2_0-1_0-0_0.txt')
+    print('simulation.exe',i,3,'6.0 2.0 0.0 > log/'+str(i)+'-3-6_0-2_0-0_0.txt')
